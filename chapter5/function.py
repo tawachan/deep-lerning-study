1d7885a0e45d914ada42510818eb2fe482ea75f5
Yusuke Otawa / tawachan <otawa.yusuke@gmail.com>
2020-07-19 19:25:37 +0900

    fix

--- chapter5/function.py
+++ chapter5/function.py
@@ -1,5 +1,8 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+
+
+def identity_function(x):
+    return x
 
 
 def step_function(x):
@@ -7,59 +10,96 @@ def step_function(x):
 
 
 def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
 
 
 def relu(x):
     return np.maximum(0, x)
 
 
-def identity_function(x):
-    return x
+def relu_grad(x):
+    grad = np.zeros_like(x)
+    grad[x >= 0] = 1
+    return grad
 
 
 def softmax(x):
-    c = np.max(x)
-    exp_a = np.exp(x - c)
-    sum_exp_a = np.sum(exp_a)  # オーバーフロー対策
-    return exp_a / sum_exp_a
+    x = x - np.max(x, axis=-1, keepdims=True)   # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+
+def sum_squared_error(y, t):
+    return 0.5 * np.sum((y - t)**2)
 
 
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
+
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
     batch_size = y.shape[0]
-    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 
-def numerical_gradient(f, X):
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
+
+
+def _numerical_gradient_1d(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val  # 値を元に戻す
+
+    return grad
+
+
+def numerical_gradient_2d(f, X):
     if X.ndim == 1:
-        return numerical_gradient_1d(f, X)
+        return _numerical_gradient_1d(f, X)
     else:
         grad = np.zeros_like(X)
 
         for idx, x in enumerate(X):
-            grad[idx] = numerical_gradient_1d(f, x)
+            grad[idx] = _numerical_gradient_1d(f, x)
 
         return grad
 
 
-def numerical_gradient_1d(f, x):
-    h = 1e-4
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
     grad = np.zeros_like(x)
 
-    for i in range(x.shape[0]):
-        original_value = x[i]
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)  # f(x+h)
 
-        x[i] = original_value + h
-        fx_1 = f(x)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
 
-        x[i] = original_value - h
-        fx_2 = f(x)
-
-        grad[i] = (fx_1 - fx_2) / 2 * h
-
-        x[i] = original_value
+        x[idx] = tmp_val  # 値を元に戻す
+        it.iternext()
 
     return grad
